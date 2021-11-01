acc3af1070088d5b3e3c30efb8772174d1b75d0c
Luis <iluisflores@yahoo.com>
2021-11-01 13:36:03 -0500

    "created new firebase app and firebaseConfig"

---
+++ src/services/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCcfvchmp8Tt5rVsGOnYGXtubaN84T8_b0",
+    authDomain: "free-games-collection.firebaseapp.com",
+    projectId: "free-games-collection",
+    storageBucket: "free-games-collection.appspot.com",
+    messagingSenderId: "449229718424",
+    appId: "1:449229718424:web:8e9d2afc1960192ae861ed",
+    measurementId: "G-1NVF689FK9"
+  };
+
+  firebase.initializeApp(firebaseConfig);
