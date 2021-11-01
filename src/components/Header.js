9ef9af1747d42367a613a35f4289e0b1e72921a8
Luis <iluisflores@yahoo.com>
2021-11-01 14:55:05 -0500

    "styled header"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,21 @@
+import {Link} from 'react-router-dom';
+import { logOut} from '../services/firebase';
+import {StyledHeader} from '../styles';
+
 const Header = (props) => {
-    return <h1>Header</h1>
+    return (
+        <StyledHeader>
+            <h1>Free-Games-Collection</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link>Login</Link>
+                    </li>
+                    <li onClick={logOut}>Logout</li>
+                </ul>
+            </nav>
+        </StyledHeader>
+    )
 }
 
 export default Header;
---
+++ src/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+
+export const StyledHeader = styled.header `
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding-left: 1rem;
+        height: 5rem;
+        background-color: black;
+        color: white;
+        box-shadow: 1px 1px 3px 2px #808080;
+        nav {
+            display: flex;
+            align-items: center;
+            ul {
+                display: flex;
+                list-style: none;
+                li{
+                    margin-right: 1.5rem;
+                    font-size: 1.5rem;
+                    font-weight: 700;
+                    a{
+                        text-decoration: none;
+                        color: inherit;
+                    }
+                    &:last-child:hover{
+                        cursor: pointer;
+                    }
+                }
+            }
+        }
+    `;
